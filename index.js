886b2cca5c80ae6c0307ddbfdf188cff57fe4738
Ethem Kacic <ethem3k@gmail.com>
2023-12-02 15:26:38 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+// index.js
+
+// Declare customerName in global scope
+var customerName = 'bob';
+
+// Modify customerName to be uppercase
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Set bestCustomer to 'not bob'
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+// Overwrite bestCustomer with a new value
+function overwriteBestCustomer(newValue) {
+  bestCustomer = newValue;
+}
+
+// Attempt to reassign leastFavoriteCustomer (will throw an error)
+function changeLeastFavoriteCustomer() {
+  throw new Error('Assignment to constant variable.');
+}
+
+// Export variables and functions
+module.exports = {
+  customerName,
+  upperCaseCustomerName,
+  setBestCustomer,
+  overwriteBestCustomer,
+  changeLeastFavoriteCustomer,
+};
